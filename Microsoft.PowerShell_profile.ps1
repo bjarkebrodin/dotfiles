116ab40bcd2462ed12c49e0453259c7e2df1f203
bjal <bjal@itu.dk>
2020-12-30 21:49:15 +0100

    add powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Import-Module pure-pwsh
+Import-Module posh-git
+
+Set-PSReadlineOption -EditMode vi
+
+Set-Alias -Name code -Value code-insiders.cmd
+Set-Alias -Name ads -Value azuredatastudio-insiders.cmd
